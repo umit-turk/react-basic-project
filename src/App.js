0206dead59155501b76d9ed6bc3653cb2300c652
Ümit Yaşar TÜRK <uyt_turk@hotmail.com>
2021-05-05 22:34:47 +0300

    history'e ulaşmak için yaziFormunu ayarladık,herhangi bir hata alırsak yazıformunda hata belirecek

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from "react-router-dom"
+
 import YaziDetayi from './components/YaziDetayi';
+import YaziEkle from './components/YaziEkle';
 import YaziListesi from './components/YaziListesi';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <div className="ui raised very padded text container segment">
           <Route path="/" exact component={YaziListesi}></Route>
           <Route path="/posts/:id" component={YaziDetayi}></Route>
+          <Route path="/yaziekle" component={YaziEkle} />
         </div>
       </div>
       </Router>
---
+++ src/components/YaziFormu.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom' // YaziFormu App.js de olmadığı için history'e ulaşmak için import ediyoruz
+
+
+const YaziFormu = (props) => {
+    const [yazi, setYazi] = useState({ title: "", content: "" }); //title ve content zorunludur.Çünkü veri girişi yapacaz.
+    const [hata, setHata] = useState("");
+
+    const onInputChange = event => setYazi({ ...yazi, [event.target.name]: event.target.value })
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        setHata("");
+        axios.post('https://react-yazi-yorum.herokuapp.com/posts', yazi) //ikinci parametre olarak yazi objesini giriyoruz
+            .then(response => {
+                props.history.push('/'); //yazımız başarılı bir şekilde yüklendiğinde anasayfaya yönlendiriyoruz.
+            })
+            .catch(error => {
+                console.log(error);
+                setHata("Başlık ve yazı içeriği alanları zorunludur.")
+            })
+    }
+
+    return (
+
+        <React.Fragment>
+            {hata &&  //eğer bir hata olursa çalışacak
+            <div class="ui error message">
+                <div class="header">Hata</div>
+                <p>{hata}</p>
+            </div>
+            }
+
+            <div className="ui form">
+                <div className="field">
+                    <label>Yazı Başlığı</label>
+
+                    <input value={YaziFormu.title} type="text" name="title" onChange={onInputChange} /> {/* name olarak title vermeliyim */}
+                </div>
+                <div className="field">
+                    <label>Yazı İçeriği</label>
+                    <textarea value={YaziFormu.content} rows="2" name="content" onChange={onInputChange}></textarea>{/* name olarak content vermeliyim */}
+                </div>
+                <button className="ui primary button" onClick={onFormSubmit}>
+                    Gönder
+            </button>
+                <button className="ui button">
+                    İptal
+            </button>
+            </div>
+        </React.Fragment>
+    )
+}
+export default withRouter(YaziFormu);
