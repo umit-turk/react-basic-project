aea6371d6a89ba24a9319a3195eac2e55469fa90
umit-turk <uyt_turk@hotmail.com>
2021-05-06 15:00:10 +0300

    redux hooks kullanımı

--- src/components/YaziDetayi.js
+++ src/components/YaziDetayi.js
@@ -2,55 +2,55 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { api } from "../api";
 import YaziYorumlari from "./YaziYorumlari";
-import { Link } from "react-router-dom";
+import { Link, useParams, useHistory, useLocation } from "react-router-dom";
 import SilModal from "./SilModal";
 
+const YaziDetayi = () => {
+  const [yaziDetayi, setYaziDetayi] = useState({});
+  const [yorumlar, setYorumlar] = useState([]);
 
-const YaziDetayi = (props) => {
-    const { id } = props.match.params; //aynı isimle bir objenin değeri varsa onu destructure yapabiliriz.
-    const [yaziDetayi, setYaziDetayi] = useState({});
-    const [yorumlar, setYorumlar] = useState([]);
+  const { id } = useParams();
+  const history = useHistory();
+  const location = useLocation();
 
+  const handleCommentSubmit = (event, yorum) => {
+    event.preventDefault();
+    api()
+      .post(`/posts/${id}/comments`, yorum)
+      .then((response) => {
+        setYorumlar([...yorumlar, response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  useEffect(() => {
+    axios
+      .all([api().get(`/posts/${id}`), api().get(`/posts/${id}/comments`)])
+      .then((responses) => {
+        setYaziDetayi(responses[0].data);
+        setYorumlar(responses[1].data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const handleCommentSubmit = (event, yorum) => {
-        event.preventDefault();
-        api()
-            .post(`/posts/${id}/comments`, yorum)
-            .then((response) => {
-                setYorumlar([...yorumlar, response.data]);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    useEffect(() => {
-        axios
-            .all([api().get(`/posts/${id}`), api().get(`/posts/${id}/comments`)])
-            .then((responses) => {
-                setYaziDetayi(responses[0].data);
-                setYorumlar(responses[1].data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-
-    return (
-
-        <React.Fragment>
-            <h2 className="ui header">{yaziDetayi.title}</h2>
-            <p>{yaziDetayi.created_at}</p>
-            <div className="ui buttons">
-                <Link className="ui blue button" to={`/posts/${yaziDetayi.id}/edit`}>Düzenle</Link>
-               {/*  <button className="ui red button">Sil</button> */}
-               <SilModal yazi={yaziDetayi} push={props.history.push}/>
-            </div>
-            <p>{yaziDetayi.content}</p>
-            <YaziYorumlari yorumlar={yorumlar} handleSubmit={handleCommentSubmit} />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <h2 className="ui header">{yaziDetayi.title}</h2>
+      <p>{yaziDetayi.created_at}</p>
+      <div className="ui buttons">
+        <Link className="ui blue button" to={`/posts/${yaziDetayi.id}/edit`}>
+          Düzenle
+        </Link>
+        <SilModal yazi={yaziDetayi} push={history.push} />
+      </div>
+      <p>{yaziDetayi.content}</p>
+      <YaziYorumlari yorumlar={yorumlar} handleSubmit={handleCommentSubmit} />
+    </React.Fragment>
+  );
 };
 
 export default YaziDetayi;
--- src/components/YaziFormu.js
+++ src/components/YaziFormu.js
@@ -1,79 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import { withRouter } from 'react-router-dom' // YaziFormu App.js de olmadığı için history'e ulaşmak için import ediyoruz
-import { api } from '../api';
-
+import React, { useState, useEffect } from "react";
+import { api } from "../api";
+import { useParams, useHistory } from "react-router-dom";
 
 const YaziFormu = (props) => {
-    const [yazi, setYazi] = useState({ title:"", content: "" }); //title ve content zorunludur.Çünkü veri girişi yapacaz.
-    const [hata, setHata] = useState("");
+  const [yazi, setYazi] = useState({
+    title: "",
+    content: "",
+  });
+  const [hata, setHata] = useState("");
 
-    console.log("YaziFormu yazi", yazi);
+  const { id } = useParams();
+  const history = useHistory();
 
-    const onInputChange = (event) => setYazi({ ...yazi, [event.target.name]: event.target.value });
+  const onInputChange = (event) =>
+    setYazi({ ...yazi, [event.target.name]: event.target.value });
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        setHata("");
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setHata("");
 
-        if(props.yazi.title){
-            //edit işlemi(yazı düzenleme işlemi varsa)
-            api().put(`/posts/${props.match.params.id}`, yazi)
-            .then(response => {
-                console.log(response);
-                props.history.push(`/posts/${props.match.params.id}`);
-            })
-            .catch(error => {
-                setHata("Başlık ve yazı içeriği alanları zorunludur.")
-            })
+    if (props.yazi?.title) {
+      api()
+        .put(`/posts/${id}`, yazi)
+        .then((response) => {
+          console.log(response);
+          history.push(`/posts/${id}`);
+        })
+        .catch((error) => {
+          setHata("Başlık ve yazı içeriği alanları zorunludur.");
+        });
+    } else {
+      api()
+        .post("/posts", yazi)
+        .then((response) => {
+          history.push("/");
+        })
+        .catch((error) => {
+          setHata("Başlık ve yazı içeriği alanları zorunludur.");
+        });
+    }
+  };
 
-        }else {
-            /// add işlemi(yazı ekleme işlemi varsa)
-            api().post('/posts', yazi) //ikinci parametre olarak yazi objesini giriyoruz
-            .then(response => {
-                props.history.push("/"); //yazımız başarılı bir şekilde yüklendiğinde anasayfaya yönlendiriyoruz.
-            })
-            .catch(error => {
-                console.log(error);
-                setHata("Başlık ve yazı içeriği alanları zorunludur.")
-            })
-        }
+  useEffect(() => {
+    if (props.yazi?.title && props.yazi?.content) setYazi(props.yazi);
+  }, [props.yazi]);
 
-       
-        
-    };
+  return (
+    <React.Fragment>
+      {hata && (
+        <div className="ui error message">
+          <div className="header">Hata</div>
+          <p>{hata}</p>
+        </div>
+      )}
+      <div className="ui form">
+        <div className="field">
+          <label>Yazı Başlığı</label>
 
-    useEffect(() => {
-        if(props.yazi?.title && props.yazi?.content)  setYazi(props.yazi) //props.yazi? anlamaı props.yazi yoksa devamına bakmıyor.
-    }, [props.yazi])//eğer props dan gelen yazı değişirse useEffect çalışacak.
-
-    return (
-
-        <React.Fragment>
-            {hata &&  //eğer bir hata olursa çalışacak
-            <div class="ui error message">
-                <div class="header">Hata</div>
-                <p>{hata}</p>
-            </div>
-            }
-
-            <div className="ui form">
-                <div className="field">
-                    <label>Yazı Başlığı</label>
-
-                    <input value={yazi.title} type="text" name="title" onChange={onInputChange} /> {/* name olarak title vermeliyim */}
-                </div>
-                <div className="field">
-                    <label>Yazı İçeriği</label>
-                    <textarea value={yazi.content} rows="2" name="content" onChange={onInputChange}></textarea>{/* name olarak content vermeliyim */}
-                </div>
-                <button className="ui primary button" onClick={onFormSubmit}>
-                    Gönder
-            </button>
-                <button className="ui button">
-                    İptal
-            </button>
-            </div>
-        </React.Fragment>
-    );
+          <input
+            value={yazi.title}
+            type="text"
+            name="title"
+            onChange={onInputChange}
+          />
+        </div>
+        <div className="field">
+          <label>Yazı İçeriği</label>
+          <textarea
+            value={yazi.content}
+            rows="3"
+            name="content"
+            onChange={onInputChange}
+          ></textarea>
+        </div>
+        <button className="ui primary button" onClick={onFormSubmit}>
+          Gönder
+        </button>
+        <button className="ui button">İptal Et</button>
+      </div>
+    </React.Fragment>
+  );
 };
-export default withRouter(YaziFormu);
+
+export default YaziFormu;
